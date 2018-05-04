22a4a92a100caa982c8fe451a4a786dcbb2a2294
shadowmoose <theshadowmoose@gmail.com>
2018-05-03 17:52:00 -0700

    Version detection.

---
+++ patchalerts/util/version.py
@@ -0,0 +1,33 @@
+import os
+import subprocess
+
+
+# Return the git revision as a string
+def git_version():
+	def _minimal_ext_cmd(cmd):
+		# construct minimal environment
+		env = {}
+		for k in ['SYSTEMROOT', 'PATH']:
+			v = os.environ.get(k)
+			if v is not None:
+				env[k] = v
+		# LANGUAGE is used on win32
+		env['LANGUAGE'] = 'C'
+		env['LANG'] = 'C'
+		env['LC_ALL'] = 'C'
+		_out = subprocess.Popen(cmd, stdout=subprocess.PIPE, env=env,
+								cwd=os.path.abspath(os.path.dirname(os.path.realpath(__file__)))).communicate()[0]
+		return _out
+
+	try:
+		out = _minimal_ext_cmd(['git', 'rev-parse', '--short', 'HEAD'])
+		git_revision = out.strip().decode('ascii')
+		if len(git_revision) == 0:
+			raise OSError()
+	except OSError:
+		git_revision = "Unknown"
+
+	return git_revision
+
+
+print('Version:', git_version())
