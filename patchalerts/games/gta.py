f138a321962a44210e365fbb7c3094a2aee9019d
ShadowMoose <theshadowmoose@gmail.com>
2018-11-04 17:29:53 -0700

    Fix for GTA V.

--- patchalerts/games/gta.py
+++ patchalerts/games/gta.py
@@ -10,17 +10,16 @@ class GTA5(Site):
 
 	def scan(self):
 		dat = loader.json(
-			'https://www.rockstargames.com/rockstarsupport2a/knowledge-base/get-section/115002861208.json?locale=en-us')
-		articles = dat['section']['articles']
-		for a in articles:
-			soup = loader.direct_soup(a['body'])
-			_title = a['title']
-			_url = 'https://support.rockstargames.com/hc/articles/%s' % a['id']
-			_desc = soup.getText('\n').strip()
-			if len(_desc.split('\n')) > 1:  # Quick-n-dirty fix to remove the embedded title header.
-				_desc = _desc.split('\n', 1)[1].strip()
-			_desc = _desc.replace('\n\n', '\n').replace('\n\n', '\n')
-			yield Update(game=self.name, update_name=_title, post_url=_url, desc=_desc, thumb=self.icon, color="#588622")
+			'https://www.rockstargames.com/newswire/tags.json?tags=591&page=1')
+		posts = dat['posts']
+		for a in posts:
+			_title = a['title'].split('<')[0]
+			_url = a['link']
+			_desc = a['blurb'].split('<')[0].replace('\n\n', '\n').replace('\n\n', '\n')
+			_image = None
+			if 'preview_images_parsed' in a and 'featured' in a['preview_images_parsed']:
+				_image = a['preview_images_parsed']['featured']['src']
+			yield Update(game=self.name, update_name=_title, post_url=_url, desc=_desc, thumb=self.icon, color="#588622", image=_image)
 
 
 if __name__ == "__main__":
