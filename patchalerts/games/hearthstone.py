3343bdb93376a6bc26196cb3f66c67f1f9696397
ShadowMoose <theshadowmoose@gmail.com>
2020-03-18 21:24:46 -0700

    Fixed Hearthstone.

--- patchalerts/games/hearthstone.py
+++ patchalerts/games/hearthstone.py
@@ -8,15 +8,17 @@ class Hearthstone(Game):
 		super().__init__('Hearthstone', homepage='https://playhearthstone.com/')
 
 	def scan(self):
-		soup = loader.soup("https://us.battle.net/forums/en/hearthstone/22814011/")
-		table = soup.find(attrs={"class": 'Forum-ForumTopicList'})
-		elems = table.find_all(attrs={'class': 'ForumTopic'})
+		soup = loader.soup("https://playhearthstone.com/en-us/blog//infinite?page=1&articleType=blog")
+		print('Soup:', soup)
+		elems = soup.find_all('li')
 		for elem in elems:
-			dsc = elem.find(attrs={"class": 'ForumTopic-title'})
-			_url = 'https://us.battle.net' + elem["href"]
-			_title = dsc.text
-			_desc = dsc['data-tooltip-content']
-			yield Update(game=self, update_name=_title, post_url=_url, desc=_desc, color="#6785c2")
+			a = elem.find(attrs={"class": "article-title"}).find(attrs={'class': 'media--link'})
+			dsc = elem.find(attrs={"class": 'article-summary'})
+			_url = 'https://playhearthstone.com' + a["href"]
+			_title = a.text
+			_desc = dsc.text
+			_img = 'https:' + elem.find('img')['data-src']
+			yield Update(game=self, update_name=_title, post_url=_url, desc=_desc, color="#6785c2", image=_img)
 
 
 if __name__ == "__main__":
